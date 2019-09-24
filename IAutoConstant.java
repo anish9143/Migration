a6f24e7deb93ec5bdf571cd2e8ff5392b637d464
Anish Thakur <48209887+anish9143@users.noreply.github.com>
2019-09-24 14:24:55 +0530

    Create IAutoConstant.java

---
+++ IAutoConstant.java
@@ -0,0 +1,15 @@
+package com.granicus.migration.generic;
+
+public interface IAutoConstant {
+
+	public static final String Sites_CSV_PATH = ".\\Sites.csv";
+	public static final String Version_CSV_PATH = ".\\version.csv";
+	public static final String GECKO_KEY = "webdriver.gecko.driver";
+	public static final String GECKO_VALUE = ".\\Driver\\geckodriver.exe";
+	public static final String CHROME_KEY = "webdriver.chrome.driver";
+	public static final String CHROME_VALUE = ".\\Driver\\chromedriver.exe";
+	public static final String IE_KEY = "webdriver.ie.driver";
+	public static final String IE_VALUE = ".\\Driver\\IEDriverServer.exe";
+	public static final String EDGE_KEY = "webdriver.edge.driver";
+	public static final String EDGE_VALUE = ".\\Driver\\MicrosoftWebDriver.exe";
+}
